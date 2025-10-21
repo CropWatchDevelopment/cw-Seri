4e945f4fa6d830b728adb8ff14ac5bbdb6dc4814
Uri Tadmore <taduri.fwdev@outlook.com>
2025-10-22 01:05:45 +1100

    - add tasks as well as a message queue all with static allocation.

--- Core/Src/freertos.c
+++ Core/Src/freertos.c
@@ -47,14 +47,30 @@
 /* USER CODE BEGIN Variables */
 
 /* USER CODE END Variables */
-osThreadId defaultTaskHandle;
+osThreadId startupTaskHandle;
+uint32_t startupTaskBuffer[ 64 ];
+osStaticThreadDef_t startupTaskControlBlock;
+osThreadId healthCheckHandle;
+uint32_t healthCheckBuffer[ 64 ];
+osStaticThreadDef_t healthCheckControlBlock;
+osThreadId LoRaTxHandle;
+uint32_t LoRaTxBuffer[ 64 ];
+osStaticThreadDef_t LoRaTxControlBlock;
+osMessageQId LoRaTxQHandle;
+uint8_t LoRaTxQBuffer[ 4 * sizeof( uint32_t ) ];
+osStaticMessageQDef_t LoRaTxInfo;
+osMessageQId LoRaRxQHandle;
+uint8_t LoRaRxQBuffer[ 6 * sizeof( uint32_t ) ];
+osStaticMessageQDef_t LoRaRxQInfo;
 
 /* Private function prototypes -----------------------------------------------*/
 /* USER CODE BEGIN FunctionPrototypes */
 
 /* USER CODE END FunctionPrototypes */
 
-void StartDefaultTask(void const * argument);
+void startupTaskFunc(void const * argument);
+void HealthCheckTaskFunc(void const * argument);
+void LoRaTxTaskFunc(void const * argument);
 
 void MX_FREERTOS_Init(void); /* (MISRA C 2004 rule 8.1) */
 
@@ -63,6 +79,7 @@ void vApplicationGetIdleTaskMemory( StaticTask_t **ppxIdleTaskTCBBuffer, StackTy
 
 /* Hook prototypes */
 void vApplicationIdleHook(void);
+void vApplicationTickHook(void);
 
 /* USER CODE BEGIN 2 */
 __weak void vApplicationIdleHook( void )
@@ -78,6 +95,17 @@ __weak void vApplicationIdleHook( void )
    memory allocated by the kernel to any task that has since been deleted. */
 }
 /* USER CODE END 2 */
+
+/* USER CODE BEGIN 3 */
+void vApplicationTickHook( void )
+{
+   /* This function will be called by each tick interrupt if
+   configUSE_TICK_HOOK is set to 1 in FreeRTOSConfig.h. User code can be
+   added here, but the tick hook is called from an interrupt context, so
+   code must not attempt to block, and only the interrupt safe FreeRTOS API
+   functions can be used (those that end in FromISR()). */
+}
+/* USER CODE END 3 */
 
 /* USER CODE BEGIN PREPOSTSLEEP */
 __weak void PreSleepProcessing(uint32_t *ulExpectedIdleTime)
@@ -126,14 +154,31 @@ void MX_FREERTOS_Init(void) {
   /* start timers, add new ones, ... */
   /* USER CODE END RTOS_TIMERS */
 
+  /* Create the queue(s) */
+  /* definition and creation of LoRaTxQ */
+  osMessageQStaticDef(LoRaTxQ, 4, uint32_t, LoRaTxQBuffer, &LoRaTxInfo);
+  LoRaTxQHandle = osMessageCreate(osMessageQ(LoRaTxQ), NULL);
+
+  /* definition and creation of LoRaRxQ */
+  osMessageQStaticDef(LoRaRxQ, 6, uint32_t, LoRaRxQBuffer, &LoRaRxQInfo);
+  LoRaRxQHandle = osMessageCreate(osMessageQ(LoRaRxQ), NULL);
+
   /* USER CODE BEGIN RTOS_QUEUES */
   /* add queues, ... */
   /* USER CODE END RTOS_QUEUES */
 
   /* Create the thread(s) */
-  /* definition and creation of defaultTask */
-  osThreadDef(defaultTask, StartDefaultTask, osPriorityNormal, 0, 128);
-  defaultTaskHandle = osThreadCreate(osThread(defaultTask), NULL);
+  /* definition and creation of startupTask */
+  osThreadStaticDef(startupTask, startupTaskFunc, osPriorityNormal, 0, 64, startupTaskBuffer, &startupTaskControlBlock);
+  startupTaskHandle = osThreadCreate(osThread(startupTask), NULL);
+
+  /* definition and creation of healthCheck */
+  osThreadStaticDef(healthCheck, HealthCheckTaskFunc, osPriorityAboveNormal, 0, 64, healthCheckBuffer, &healthCheckControlBlock);
+  healthCheckHandle = osThreadCreate(osThread(healthCheck), NULL);
+
+  /* definition and creation of LoRaTx */
+  osThreadStaticDef(LoRaTx, LoRaTxTaskFunc, osPriorityHigh, 0, 64, LoRaTxBuffer, &LoRaTxControlBlock);
+  LoRaTxHandle = osThreadCreate(osThread(LoRaTx), NULL);
 
   /* USER CODE BEGIN RTOS_THREADS */
   /* add threads, ... */
@@ -141,22 +186,58 @@ void MX_FREERTOS_Init(void) {
 
 }
 
-/* USER CODE BEGIN Header_StartDefaultTask */
+/* USER CODE BEGIN Header_startupTaskFunc */
 /**
-  * @brief  Function implementing the defaultTask thread.
+  * @brief  Function implementing the startupTask thread.
   * @param  argument: Not used
   * @retval None
   */
-/* USER CODE END Header_StartDefaultTask */
-void StartDefaultTask(void const * argument)
+/* USER CODE END Header_startupTaskFunc */
+void startupTaskFunc(void const * argument)
 {
-  /* USER CODE BEGIN StartDefaultTask */
+  /* USER CODE BEGIN startupTaskFunc */
   /* Infinite loop */
   for(;;)
   {
     osDelay(1);
   }
-  /* USER CODE END StartDefaultTask */
+  /* USER CODE END startupTaskFunc */
+}
+
+/* USER CODE BEGIN Header_HealthCheckTaskFunc */
+/**
+* @brief Function implementing the healthCheck thread.
+* @param argument: Not used
+* @retval None
+*/
+/* USER CODE END Header_HealthCheckTaskFunc */
+void HealthCheckTaskFunc(void const * argument)
+{
+  /* USER CODE BEGIN HealthCheckTaskFunc */
+  /* Infinite loop */
+  for(;;)
+  {
+    osDelay(1);
+  }
+  /* USER CODE END HealthCheckTaskFunc */
+}
+
+/* USER CODE BEGIN Header_LoRaTxTaskFunc */
+/**
+* @brief Function implementing the LoRaTx thread.
+* @param argument: Not used
+* @retval None
+*/
+/* USER CODE END Header_LoRaTxTaskFunc */
+void LoRaTxTaskFunc(void const * argument)
+{
+  /* USER CODE BEGIN LoRaTxTaskFunc */
+  /* Infinite loop */
+  for(;;)
+  {
+    osDelay(1);
+  }
+  /* USER CODE END LoRaTxTaskFunc */
 }
 
 /* Private application code --------------------------------------------------*/
